48bc321a3b739b0bbb5bb748ed7aeefa7ae1f7d1
xuhao <475152963@qq.com>
2020-05-04 13:26:19 +0800

    详情查看

--- src/views/directory/arrangement/index.vue
+++ src/views/directory/arrangement/index.vue
@@ -49,7 +49,31 @@
         </el-table-column>
         <el-table-column label="操作" width="200" align="center">
           <template slot-scope="scope">
-            <el-button size="mini" @click="handleDetail(scope.$index, scope.row)" type="primary">查看详情</el-button>
+            <el-button type="text" @click="handleDetail(scope.$index, scope.row)" >查看详情</el-button>
+            <el-dialog title="目录详情" :visible.sync="dialogTableVisible">
+              <div class="form-container-border">
+                <el-row>
+                  <el-col :span="6" class="form-border form-left-bg font-small">目录编号</el-col>
+                  <el-col class="form-border font-small" :span="18">{{showData.id}}</el-col>
+                </el-row>
+                <el-row>
+                  <el-col :span="6" class="form-border form-left-bg font-medium">平台名称</el-col>
+                  <el-col class="form-border font-medium" :span="18">{{showData.name}}</el-col>
+                </el-row>
+                <el-row>
+                  <el-col :span="6" class="form-border form-left-bg font-medium">平台ip</el-col>
+                  <el-col class="form-border font-medium" :span="18">{{showData.ip}}</el-col>
+                </el-row>
+                <el-row>
+                  <el-col :span="6" class="form-border form-left-bg font-medium">目录添加时间</el-col>
+                  <el-col class="form-border font-medium" :span="18">{{showData.time}}</el-col>
+                </el-row>
+                <el-row>
+                  <el-col :span="6" class="form-border form-left-bg font-medium">目录名称</el-col>
+                  <el-col class="form-border font-medium" :span="18">{{showData.dir}}</el-col>
+                </el-row>
+              </div>
+            </el-dialog>
             <el-button size="mini" @click="handleDelete(scope.$index, scope.row)" v-show="scope.row.status===0" type="danger">删除平台</el-button>
             <el-button size="mini" @click="handleChange(scope.$index, scope.row)" v-show="scope.row.status===1" type="primary">编辑目录</el-button>
           </template>
@@ -59,7 +83,7 @@
     <div class="pagination-container">
       <el-pagination
         background
-        @size-change="handleSizeChange()"
+        @size-change="handleSizeChange"
         @current-change="handleCurrentChange()"
         layout="total, sizes,prev, pager, next,jumper"
         :current-page.sync="listQuery.pageNum"
@@ -90,12 +114,12 @@
         listQuery: Object.assign({}, defaultListQuery),
         listLoading: false,
         selectProductCateValue: null,
-        rows: '',               //总目录信息条数，执行getAllList函数的时候后端会传进来
-        pageTotal: 0,          //总页数，执行getAllList函数的时候后端会传进来
+        dialogTableVisible: false,
+        pageTotal: 0,          //总条数
         searchList: [],       //执行查询操作后，满足查询条件的所有的目录信息的数组
         allList: [            //获取的所有的目录信息数组
           {
-          id:'123',
+          id:'1',
           name:'物联网平台1',
           ip:'192.168.1.1',
           time:'2019-11-12',
@@ -103,90 +127,97 @@
           status:1
           },
           {
-            id:'124',
+            id:'2',
             name:'物联网平台2',
-            ip:'192.168.1.1',
+            ip:'192.168.1.2',
             time:'2019-11-12',
             dir:'123',
             status:0
           },
           {
-            id:'125',
+            id:'3',
             name:'物联网平台3',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.3',
+            time:'2019-11-13',
             dir:'123',
             status:0
           },
           {
-            id:'125',
+            id:'4',
             name:'物联网平台4',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.4',
+            time:'2019-11-13',
             dir:'123',
             status:1
           },
           {
-            id:'123',
+            id:'5',
             name:'物联网平台5',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.5',
+            time:'2019-11-14',
             dir:'123',
             status:1
           },
           {
-            id:'123',
+            id:'6',
             name:'物联网平台6',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.6',
+            time:'2019-11-14',
             dir:'123',
             status:1
           },
           {
-            id:'123',
+            id:'7',
             name:'物联网平台7',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.7',
+            time:'2019-11-14',
             dir:'123',
             status:1
           },{
-            id:'123',
+            id:'8',
             name:'物联网平台8',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.8',
+            time:'2019-11-16',
             dir:'123',
             status:1
           },{
-            id:'123',
+            id:'9',
             name:'物联网平台9',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.9',
+            time:'2019-11-16',
             dir:'123',
             status:1
           },{
-            id:'123',
+            id:'10',
             name:'物联网平台10',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.10',
+            time:'2019-11-17',
             dir:'123',
             status:1
           },{
-            id:'123',
+            id:'11',
             name:'物联网平台11',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.11',
+            time:'2019-11-17',
             dir:'123',
             status:1
           },{
-            id:'123',
+            id:'12',
             name:'物联网平台12',
-            ip:'192.168.1.1',
-            time:'2019-11-12',
+            ip:'192.168.1.12',
+            time:'2019-11-19',
             dir:'123',
             status:1
           }
           ],          //截取的当前要展示的目录信息数组
         list: [],
+        showData: {
+          id: '',
+          name: '',
+          ip: '',
+          time: '',
+          dir: ''
+        },
         operateType: null,
         multipleSelection: [],
         closeOrder:{
@@ -210,7 +241,6 @@
         .then(function (res) {
           console.log(res);
           this.allList = res.data.data;               //第二个data是后端传递的数组名，可能需要修改
-          this.rows = res.data.rows;                  //后面的rows是后端传递的总目录信息条数，可能需要修改
         })
         .catch(function (error) {
           console.log(error);
@@ -227,15 +257,17 @@
         this.pageTotal = this.allList.length;
         this.list = this.allList.slice(this.listQuery.pageSize*(this.listQuery.pageNum-1),this.listQuery.pageSize*this.listQuery.pageNum);
       },
-      handleSizeChange() {            //改变当前页需要展示的目录信息条数后的目录信息的变化
+      handleSizeChange(val) {            //改变当前页需要展示的目录信息条数后的目录信息的变化
         this.listQuery.pageNum = 1;
+        this.listQuery.pageSize = val;
         this.changeList();
       },
       handleCurrentChange() {            //改变当前页码后需要展示的目录信息的变化
         this.changeList();
       },
-      handleDetail(){
-
+      handleDetail(index, row){
+        this.dialogTableVisible = true;
+        this.showData = row;
       },
       handleDelete(){
 
@@ -255,5 +287,18 @@
 <style scoped>
   .input-width {
     width: 203px;
+  }
+  .form-container-border {
+    border-left: 1px solid #DCDFE6;
+    border-top: 1px solid #DCDFE6;
+    margin-top: 15px;
+  }
+  .form-border {
+    border-right: 1px solid #DCDFE6;
+    border-bottom: 1px solid #DCDFE6;
+    padding: 10px;
+  }
+  .form-left-bg {
+    background: #F2F6FC;
   }
 </style>
